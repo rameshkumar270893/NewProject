d132ad99d7e98f4837177aceeb5396ab962ae5d3
rameshkumar270893 <rameshkumar270893@gmail.com>
2022-11-12 15:58:46 +0530

    Added 2 methods from Kaushik

--- src/test/java/com/hexaware/ClassA.java
+++ src/test/java/com/hexaware/ClassA.java
@@ -21,5 +21,15 @@ public class ClassA {
 		System.out.println("4");
 
 	}
+	
+	private void method9() {
+		System.out.println("9");
+
+	}
+	
+	private void method10() {
+		System.out.println("10");
+
+	}
 
 }
