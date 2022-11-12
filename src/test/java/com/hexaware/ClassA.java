39c2c9c55b0d530246030be3f579751ff6e538f3
rameshkumar270893 <rameshkumar270893@gmail.com>
2022-11-12 15:37:13 +0530

    First time push

---
+++ src/test/java/com/hexaware/ClassA.java
@@ -0,0 +1,25 @@
+package com.hexaware;
+
+public class ClassA {
+
+	private void method1() {
+		System.out.println("1");
+
+	}
+
+	private void method2() {
+		System.out.println("2");
+
+	}
+
+	private void method3() {
+		System.out.println("3");
+
+	}
+	
+	private void method4() {
+		System.out.println("4");
+
+	}
+
+}
